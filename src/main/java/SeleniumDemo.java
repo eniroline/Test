4a7b7d2074f213f14775300cec8ceeea75ace2ed
Brahinska <OBrahinska@kiev.luxoft.com>
2018-03-28 19:06:52 +0300

    Shipping test https://jazvainc.atlassian.net/wiki/spaces/JD/pages/36306945/Incident+27+March+2018

--- src/main/java/SeleniumDemo.java
+++ src/main/java/SeleniumDemo.java
@@ -3,33 +3,29 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-//import org.testng.asserts.SoftAssert;
 
 public class SeleniumDemo {
-    //SoftAssert softAssert = new SoftAssert();
+
     public static void main(String[] args) throws InterruptedException {
 
-        //By by = By.className("b_searchbox");
-        //System.setProperty("webdriver.ie.driver", file.getAbsolutePath());
-        //WebDriver explorer = new InternetExplorerDriver();
-        //explorer.get("https://www.bing.com/");
-        //explorer.quit();
-
         System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
-        //System.getProperty("driver.chrome");
         WebDriver chrome = new ChromeDriver();
         chrome.get("https://master.jazva.com/login.jsp");
-        //chrome.wait(10);
         WebElement uname = chrome.findElement(By.xpath("//*[@id=\"username\"]"));
         uname.clear();
         uname.sendKeys("test");
         WebElement pass = chrome.findElement(By.xpath("//*[@id=\"password\"]"));
         pass.clear();
         pass.sendKeys("123456jz");
-        //chrome.findElement(By.className("btn btn-primary")).click();
         chrome.findElement(By.xpath("//*[@id=\"login\"]/div[2]/div[2]/div[2]/div[1]/form/div[3]/input")).click();
         chrome.findElement(By.xpath("//*[@id=\"mainnav\"]/ul/li[2]/a/span[1]")).click();
-        Assert.assertTrue("Text 'Контактная информация' is absent", chrome.getPageSource().contains("Tasks"));
+        Assert.assertFalse("Error on page", chrome.getPageSource().contains("Error"));
+        Assert.assertTrue("Text 'Tasks' is absent", chrome.getPageSource().contains("Tasks"));
+        Assert.assertTrue("Text 'Alerts", chrome.getPageSource().contains("Alerts"));
+        
+        chrome.findElement(By.xpath("//*[@id=\"mainnav\"]/ul/li[2]/div/ul/li[11]/a")).click();
+        Assert.assertFalse("Error on the page 'Orders - Shipping'", chrome.getPageSource().contains("Error"));
+        Assert.assertTrue("Text 'Tasks' is absent", chrome.getPageSource().contains("Shipment"));
         //chrome.wait(10);
         chrome.close();
         chrome.quit();
